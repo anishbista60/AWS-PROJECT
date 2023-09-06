cf212fb72aef397f5a8d1c6991aba3966f354ce9
anishbista60 <108048384+anishbista60@users.noreply.github.com>
2023-09-06 13:01:08 +0530

    Update app.py

--- aws_pipeline_implementation/app.py
+++ aws_pipeline_implementation/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! and Congrats you are sucessfully done with this project '
+    return 'Hello, world! and Congrats you are sucessfully done with this  '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
